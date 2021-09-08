e0c7ee95994034d6c90bc71899aef87fc441beac
Abdulkarim <Abdulkarim.Alansari@outlook.com>
2021-09-08 23:55:38 +0300

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Abdulkarim Alansari on 01/02/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
